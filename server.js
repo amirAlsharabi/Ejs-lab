9bc7648ebf76a1104fb4934e2393898319e34982
amir <amiralsharabi2@gmail.com>
2026-09-01 14:18:26 +0300

    done

--- server.js
+++ server.js
@@ -1,127 +1,102 @@
-const express = require("express") //importing express package
-const app = express() // creates a express application
+const express = require("express"); //importing express package
+const app = express(); // creates a express application
 
-app.use(express.static('public')); //all static files are in the public folder
-
-
-
-
+app.use(express.static("public")); //all static files are in the public folder
 
 // DATABASE
 const resturaunts = [
   {
     id: 1,
-    name: 'Al Baik',
+    name: "Al Baik",
     isOpen: true,
-    address: 'City Centre Road 4650, Manama',
-    phone: '55509876',
+    address: "City Centre Road 4650, Manama",
+    phone: "55509876",
     menu: [
       {
-        name: 'Chicken Filtet Sandwich',
-        price: .750,
-        rating: 5
+        name: "Chicken Filtet Sandwich",
+        price: 0.75,
+        rating: 5,
       },
       {
-        name: 'Fish Filet Sandwich',
-        price: .900,
-        rating: 5
+        name: "Fish Filet Sandwich",
+        price: 0.9,
+        rating: 5,
       },
       {
-        name: 'Chicken Nuggets',
+        name: "Chicken Nuggets",
         price: 1.8,
-        rating: 4.9
+        rating: 4.9,
       },
-        ]
+    ],
   },
   {
     id: 2,
     name: "Haji's Traditional Cafe",
     isOpen: false,
-    address: '150 شارع الحكومة, Manama',
-    phone: '06892543',
+    address: "150 شارع الحكومة, Manama",
+    phone: "06892543",
     menu: [
       {
-        name: 'Chicken Machboos',
+        name: "Chicken Machboos",
         price: 2.2,
         rating: 4.5,
       },
-            {
-        name: 'Balaleet',
+      {
+        name: "Balaleet",
         price: 1.8,
         rating: 5,
       },
       {
-        name: 'Cheese Omlette',
-        price: .7,
+        name: "Cheese Omlette",
+        price: 0.7,
         rating: 5,
       },
-          ]
+    ],
   },
   {
     id: 3,
-    name: 'Al Jabriya Turkish Restaurant',
+    name: "Al Jabriya Turkish Restaurant",
     isOpen: true,
-    address: 'Rd No 2643, Busaiteen',
-    phone: '17330108',
+    address: "Rd No 2643, Busaiteen",
+    phone: "17330108",
     menu: [
       {
-        name: 'Roosy Sarookh',
+        name: "Roosy Sarookh",
         price: 1.8,
         rating: 5,
       },
       {
-        name: 'Cheese Pizza',
+        name: "Cheese Pizza",
         price: 2.5,
         rating: 3.5,
       },
       {
-        name: 'French Fries',
+        name: "French Fries",
         price: 1.2,
         rating: 4.3,
       },
-        ]
-  }
+    ],
+  },
 ];
 
+app.get("/", (req, res) => {
+  res.render("homepage.ejs");
+});
 
+app.get("/resturaunts", (req, res) => {
+  res.render("all-resturaunts.ejs", {
+    resturaunts: resturaunts,
+  });
+});
 
+app.get("/resturaunts/:id", (req, res) => {
+  console.log(req.params.id);
+  const resturaunt = resturaunts.find((resid) => {
+    return Number(req.params.id) === resid.id;
+  });
+  res.render("resturaunts-details.ejs", { resturaunt: resturaunt });
+});
 
-
-
-app.get('/',(req,res)=> {
-    res.render('homepage.ejs')
-})
-
-app.get('/resturaunts',(req,res)=> {
-    res.render('all-resturaunts.ejs',{
-      resturaunts : resturaunts
-    }
-     )
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(3000,()=>{
-    console.log('App is Running')
-}) // listen on port 3000
-
-
-
-
-
-
-
+app.listen(3000, () => {
+  console.log("App is Running");
+}); // listen on port 3000
